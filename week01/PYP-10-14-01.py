711fdd02d85c80f7d159c89fc647f5e0efd3505f
Yuriko Goto <gotoyuriko95@gmail.com>
2020-10-14 18:56:02 +0900

    🐍

---
+++ week01/PYP-10-14-01.py
@@ -0,0 +1,8 @@
+print("Hello")
+# print(5+7)
+user_name = input("What is your name?")
+print("what is your birth year?")
+birth_year = int(input())
+age = 2020 - birth_year
+print("\n\nHello" + user_name + "! You are" + str(age) + "this year.")
+# print(age)
---
+++ week01/week1.md
@@ -0,0 +1,10 @@
+###### tags: 'python'
+> [time=Wed, Oct 14, 2020 9:17 AM]
+
+# week1 
+* Solving problem using Algorithm.
+* Algorithm
+    * Pseudocode
+    * Flowchart
+
+* [Python 3.9.0 Install](https://www.python.org/downloads/release/python-390/)
